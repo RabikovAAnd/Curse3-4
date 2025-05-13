87b073a21fa28bab7bd9e81c9b7c1827d2912461
RabikovAAnd <randr.one@yandex.ru>
2025-05-13 10:01:21 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnRectangle = New System.Windows.Forms.Button()
+        Me.lblPerimeter = New System.Windows.Forms.Label()
+        Me.lblArea = New System.Windows.Forms.Label()
+        Me.btnTax = New System.Windows.Forms.Button()
+        Me.lblTaxAmount = New System.Windows.Forms.Label()
+        Me.lblDiscount = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnRectangle
+        '
+        Me.btnRectangle.Location = New System.Drawing.Point(12, 12)
+        Me.btnRectangle.Name = "btnRectangle"
+        Me.btnRectangle.Size = New System.Drawing.Size(200, 40)
+        Me.btnRectangle.TabIndex = 0
+        Me.btnRectangle.Text = "Создать прямоугольник"
+        Me.btnRectangle.UseVisualStyleBackColor = True
+        '
+        'lblPerimeter
+        '
+        Me.lblPerimeter.AutoSize = True
+        Me.lblPerimeter.Location = New System.Drawing.Point(12, 65)
+        Me.lblPerimeter.Name = "lblPerimeter"
+        Me.lblPerimeter.Size = New System.Drawing.Size(62, 13)
+        Me.lblPerimeter.TabIndex = 1
+        Me.lblPerimeter.Text = "Периметр:"
+        '
+        'lblArea
+        '
+        Me.lblArea.AutoSize = True
+        Me.lblArea.Location = New System.Drawing.Point(12, 88)
+        Me.lblArea.Name = "lblArea"
+        Me.lblArea.Size = New System.Drawing.Size(57, 13)
+        Me.lblArea.TabIndex = 2
+        Me.lblArea.Text = "Площадь:"
+        '
+        'btnTax
+        '
+        Me.btnTax.Location = New System.Drawing.Point(12, 120)
+        Me.btnTax.Name = "btnTax"
+        Me.btnTax.Size = New System.Drawing.Size(200, 40)
+        Me.btnTax.TabIndex = 3
+        Me.btnTax.Text = "Рассчитать транспортный налог"
+        Me.btnTax.UseVisualStyleBackColor = True
+        '
+        'lblTaxAmount
+        '
+        Me.lblTaxAmount.AutoSize = True
+        Me.lblTaxAmount.Location = New System.Drawing.Point(12, 173)
+        Me.lblTaxAmount.Name = "lblTaxAmount"
+        Me.lblTaxAmount.Size = New System.Drawing.Size(75, 13)
+        Me.lblTaxAmount.TabIndex = 4
+        Me.lblTaxAmount.Text = "Сумма налога"
+        '
+        'lblDiscount
+        '
+        Me.lblDiscount.AutoSize = True
+        Me.lblDiscount.Location = New System.Drawing.Point(12, 196)
+        Me.lblDiscount.Name = "lblDiscount"
+        Me.lblDiscount.Size = New System.Drawing.Size(42, 13)
+        Me.lblDiscount.TabIndex = 5
+        Me.lblDiscount.Text = "Льгота"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Location = New System.Drawing.Point(12, 219)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(80, 13)
+        Me.lblTotal.TabIndex = 6
+        Me.lblTotal.Text = "Итого к оплате"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 261)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.lblDiscount)
+        Me.Controls.Add(Me.lblTaxAmount)
+        Me.Controls.Add(Me.btnTax)
+        Me.Controls.Add(Me.lblArea)
+        Me.Controls.Add(Me.lblPerimeter)
+        Me.Controls.Add(Me.btnRectangle)
+        Me.Name = "Form1"
+        Me.Text = "ООП - Вариант 1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnRectangle As Button
+    Friend WithEvents lblPerimeter As Label
+    Friend WithEvents lblArea As Label
+    Friend WithEvents btnTax As Button
+    Friend WithEvents lblTaxAmount As Label
+    Friend WithEvents lblDiscount As Label
+    Friend WithEvents lblTotal As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,157 @@
+﻿Imports System.Drawing
+Imports System.Windows.Forms
+
+Public Class Form1
+    Private Sub btnRectangle_Click(sender As Object, e As EventArgs) Handles btnRectangle.Click
+        ' Создаем прямоугольник
+        Dim rect As New Rectangle(
+            New Point(10, 10),
+            New Point(100, 60),
+            Color.Red,
+            Color.Yellow)
+
+        ' Вычисляем периметр и площадь
+        lblPerimeter.Text = "Периметр: " & rect.CalculatePerimeter().ToString()
+        lblArea.Text = "Площадь: " & rect.CalculateArea().ToString()
+
+        ' Рисуем прямоугольник
+        rect.Draw(Me)
+    End Sub
+
+    Private Sub btnTax_Click(sender As Object, e As EventArgs) Handles btnTax.Click
+        ' Создаем объект транспортного налога
+        Dim tax As New TransportTax(
+            "Москва",
+            2023,
+            "Легковой автомобиль",
+            150,
+            12,
+            True)
+
+        ' Рассчитываем налог
+        Dim taxAmount As Decimal = tax.CalculateTax()
+        Dim discount As Decimal = tax.CalculateDiscount()
+
+        lblTaxAmount.Text = "Сумма налога: " & taxAmount.ToString("C")
+        lblDiscount.Text = "Льгота: " & discount.ToString("C")
+        lblTotal.Text = "Итого к оплате: " & (taxAmount - discount).ToString("C")
+    End Sub
+End Class
+
+' Класс Прямоугольник
+Public Class Rectangle
+    Public Property TopLeft As Point
+    Public Property BottomRight As Point
+    Public Property BorderColor As Color
+    Public Property FillColor As Color
+
+    Public ReadOnly Property Width As Integer
+        Get
+            Return BottomRight.X - TopLeft.X
+        End Get
+    End Property
+
+    Public ReadOnly Property Height As Integer
+        Get
+            Return BottomRight.Y - TopLeft.Y
+        End Get
+    End Property
+
+    Public Sub New(topLeft As Point, bottomRight As Point, borderColor As Color, fillColor As Color)
+        Me.TopLeft = topLeft
+        Me.BottomRight = bottomRight
+        Me.BorderColor = borderColor
+        Me.FillColor = fillColor
+    End Sub
+
+    Public Function CalculatePerimeter() As Integer
+        Return 2 * (Width + Height)
+    End Function
+
+    Public Function CalculateArea() As Integer
+        Return Width * Height
+    End Function
+
+    Public Sub Draw(form As Form)
+        Dim g As Graphics = form.CreateGraphics()
+
+        ' Рисуем заливку
+        Using brush As New SolidBrush(FillColor)
+            g.FillRectangle(brush, TopLeft.X, TopLeft.Y, Width, Height)
+        End Using
+
+        ' Рисуем границу
+        Using pen As New Pen(BorderColor, 2)
+            g.DrawRectangle(pen, TopLeft.X, TopLeft.Y, Width, Height)
+        End Using
+    End Sub
+End Class
+
+' Класс Транспортный налог
+Public Class TransportTax
+    Public Property Region As String
+    Public Property Year As Integer
+    Public Property VehicleType As String
+    Public Property Power As Integer ' в л.с.
+    Public Property Period As Integer ' в месяцах
+    Public Property HasDiscount As Boolean
+
+    Public Sub New(region As String, year As Integer, vehicleType As String, power As Integer, period As Integer, hasDiscount As Boolean)
+        Me.Region = region
+        Me.Year = year
+        Me.VehicleType = vehicleType
+        Me.Power = power
+        Me.Period = period
+        Me.HasDiscount = hasDiscount
+    End Sub
+
+    Public Function CalculateTax() As Decimal
+        ' Базовая ставка (руб/л.с.)
+        Dim baseRate As Decimal = 0
+
+        ' Определяем ставку в зависимости от региона
+        Select Case Region.ToLower()
+            Case "москва"
+                If Power <= 100 Then
+                    baseRate = 12
+                ElseIf Power <= 150 Then
+                    baseRate = 25
+                Else
+                    baseRate = 50
+                End If
+            Case "санкт-петербург"
+                If Power <= 100 Then
+                    baseRate = 10
+                ElseIf Power <= 150 Then
+                    baseRate = 20
+                Else
+                    baseRate = 45
+                End If
+            Case Else
+                If Power <= 100 Then
+                    baseRate = 8
+                ElseIf Power <= 150 Then
+                    baseRate = 15
+                Else
+                    baseRate = 30
+                End If
+        End Select
+
+        ' Рассчитываем налог с учетом периода владения
+        Dim tax As Decimal = baseRate * Power * (Period / 12)
+        Return tax
+    End Function
+
+    Public Function CalculateDiscount() As Decimal
+        If Not HasDiscount Then Return 0
+
+        ' Размер льготы (например, 50% от суммы налога)
+        Dim discountRate As Decimal = 0.5D
+        Return CalculateTax() * discountRate
+    End Function
+
+    Public Sub Recalculate(newPower As Integer, newPeriod As Integer)
+        Power = newPower
+        Period = newPeriod
+    End Sub
+End Class
